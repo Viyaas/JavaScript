92f6e4a430ef666435715d3b5b7d1cce646b872d
Viyas <kram78150@gmail.com>
2022-06-25 15:10:42 +0530

    arithmetic

---
+++ Arithmetic.js
@@ -0,0 +1,8 @@
+let num1 = 5;
+let num2 = 50;
+let result;
+console.log((result = num1 + num2));
+console.log((result = num1 - num2));
+console.log((result = num1 * num2));
+console.log((result = num1 / num2));
+console.log((result = num1*num1*num1));
