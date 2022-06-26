497afa635106013c1404c7f4f307c062a8ed9581
Viyas <kram78150@gmail.com>
2022-06-27 00:20:49 +0530

    word blanks

---
+++ example.js
@@ -0,0 +1,9 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = " dumbb " + myNoun + "so" + myVerb + " so " + myAdjective + " and " + myAdverb + " thats ";; // Change this line
+// Only change code above this line
+console.log(wordBlanks);
