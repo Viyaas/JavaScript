89758ddd23d0137aa76d9ab21a7edbdadd07269a
Viyas <kram78150@gmail.com>
2022-06-27 04:18:36 +0530

    delete unused comments

--- example.js
+++ example.js
@@ -3,7 +3,7 @@ const myAdjective = "big";
 const myVerb = "ran";
 const myAdverb = "quickly";
 
-// Only change code below this line
+
 const wordBlanks = " dumbb " + myNoun + "so" + myVerb + " so " + myAdjective + " and " + myAdverb + " thats ";; // Change this line
-// Only change code above this line
+
 console.log(wordBlanks);
