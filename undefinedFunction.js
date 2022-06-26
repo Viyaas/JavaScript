34d689594b998c8c91042487a083989a18ce557f
Viyas <kram78150@gmail.com>
2022-06-27 04:20:30 +0530

    adding parameters

--- undefinedFunction.js
+++ undefinedFunction.js
@@ -1,12 +1,12 @@
 let sum = 0;
 
-function addThree()   //  fun 1
+function addThree(sum)   //  fun 1
  {
   sum = sum + 3;
 }
 
 
-function addFive()   // fun 2
+function addFive(sum)   // fun 2
 {
   sum = sum + 5;
 }
