0e3c27b9d148310bd18c6970dc76cb9e0c5f11a6
Viyas <kram78150@gmail.com>
2022-06-28 02:20:44 +0530

    using object call via dot notation

---
+++ dotNotation.js
@@ -0,0 +1,10 @@
+
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "JeansShirt",
+    "shoes": "cleats"
+  };
+  
+  
+  const hatValue = console.log(testObj.hat);     
+  const shirtValue = console.log(testObj.shirt);   
