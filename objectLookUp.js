220c3a3746bcb8c213bdfc4c102899f80df073f2
Viyas <kram78150@gmail.com>
2022-06-28 03:03:28 +0530

    still not fixed the problem

--- objectLookUp.js
+++ objectLookUp.js
@@ -1,27 +1,20 @@
-let result = "";
-
-  // Only change code below this line
-  switch(result) 
-  {
-    case "alpha":
-      result = "Adams";
-      break;
-    case "bravo":
-      result = "Boston";
-      break;
-    case "charlie":
-      result = "Chicago";
-      break;
-    case "delta":
-      result = "Denver";
-      break;
-    case "echo":
-      result = "Easy";
-      break;
-    case "foxtrot":
-      result = "Frank";
+function phoneticLookup(val) {
+    var result = "";
+    var lookup = {
+      "alpha": "Adams",
+      "bravo": "Boston",
+      "charlie": "Chicago",
+      "delta": "Denver",
+      "echo": "Easy",
+      "foxtrot": "Frank"
+    };
+  
+  
+  
+   console.log( result = lookup[val]);
+  
+    return result;
   }
 
-  // Only change code above this line
-  return result;
-console.log(result);
+  console.log(phoneticLookup());
+  
