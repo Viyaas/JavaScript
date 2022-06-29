4ce9d086422cf9099cb2c4b8b5db32eae516961e
Viyas <kram78150@gmail.com>
2022-06-29 07:49:18 +0530

    javascript array methods

---
+++ sliceMethod.js
@@ -0,0 +1,14 @@
+// function myfunction(str)
+// { let result  = " " ;
+//     while(result===str)
+//     {
+//         return result;
+//     }
+// }
+
+
+const name= ["viyas","madhu","saran"];
+// console.log(name.slice);
+
+const result = name.slice(1,0);
+console.log(result);
