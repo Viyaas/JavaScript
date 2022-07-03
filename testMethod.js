e2fe663f0a5fbd19e41d95f58248cd05ffe7c288
Viyas <kram78150@gmail.com>
2022-07-04 03:39:16 +0530

    Find the multiple test String

--- testMethod.js
+++ testMethod.js
@@ -2,3 +2,13 @@ let myString = " hi i am viyas";
 let find = /viyas/;  //  this is regular expressionn to find the word in myString
 let result = find.test(myString); // this method return true or false
 console.log(result);
+
+/* and same thing we can search multiple string ,
+using the OR operator to find the problems like above the code
+ let myString = " hi i am viyas";
+ let find = /viyas|fried|life/;   (performed in OR Operator)
+ let result = find.test(myString);
+ console.log(result);
+  
+
+*/
