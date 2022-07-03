afe770db230d6593580d1cb43cf3f4664a196574
Viyas <kram78150@gmail.com>
2022-07-04 03:33:19 +0530

    Regular Expression

---
+++ testMethod.js
@@ -0,0 +1,4 @@
+let myString = " hi i am viyas";
+let find = /viyas/;  //  this is regular expressionn to find the word in myString
+let result = find.test(myString); // this method return true or false
+console.log(result);
